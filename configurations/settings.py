3025c0c019b7b0c4fd2b42c5412964709ce9a69e
manahil <manaheal.kamran7868@gmail.com>
2024-06-13 13:32:04 +0500

    created login.html page

--- configurations/settings.py
+++ configurations/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'configurations.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
