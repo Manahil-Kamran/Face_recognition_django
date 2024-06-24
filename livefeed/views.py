0f773f2b1db78474ceb927618cc3471ea9b34f1d
manahil <manaheal.kamran7868@gmail.com>
2024-06-24 11:07:59 +0500

    Started working on "Livefeed" app by creating urls and views

--- livefeed/views.py
+++ livefeed/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 
 def LiveFeed_in(request):
     return HttpResponse("Type in to LiveFeed_in")
+def LiveFeed_out(request):
+    return HttpResponse("TYPE IN TO VIEW LIVEFEED OUT ")
