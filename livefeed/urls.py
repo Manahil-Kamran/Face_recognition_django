1b5707670ea92114a92387b19499949ef8d0b7e2
root <manaheal.kamran7868@gmail.com>
2024-08-10 02:06:05 +0500

    table created

--- livefeed/urls.py
+++ livefeed/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import LiveFeed_in, LiveFeed_out
+from .views import LiveFeedIn, LiveFeed_out
 
 
 urlpatterns = [
-    path("IN", LiveFeed_in, name="livefeedin"),
+    path("IN", LiveFeedIn.as_view(), name="livefeedin"),
     path("OUT", LiveFeed_out, name= "livefeedout"),
     
 ]
