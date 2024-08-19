4c1a5af9a90c5418f7b814052a305353d39e339f
CodeWithEhtisham <shamikhan0347@gmail.com>
2024-08-20 02:25:09 +0500

    report code updated

--- Reports/views.py
+++ Reports/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from dashboard.models import PersonAttend
+from dashboard.models import PersonAttend, PersonRegistration
 from django.utils import timezone
 from datetime import datetime
 from django.core.paginator import Paginator
+from django.db.models import Min, Max
 
 class Report(TemplateView):
     template_name = "reports/reports.html"
@@ -17,11 +18,44 @@ class Report(TemplateView):
         start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
         end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
 
-        # Filter records within the date range
-        records = PersonAttend.objects.filter(time_sent__date__range=[start_date, end_date])
-        
-        # Paginate the records
-        paginator = Paginator(records, 10)
+        # Filter and annotate records within the date range
+        first_seen = PersonAttend.objects.filter(
+            time_sent__date__range=[start_date, end_date]
+        ).values('person_id').annotate(
+            first_seen_time=Min('time_sent'),
+            last_seen_time=Max('time_sent')
+        ).order_by('first_seen_time')
+
+        # Fetch full details for the first and last seen records
+        person_data = []
+        for record in first_seen:
+            person = PersonRegistration.objects.get(id=record['person_id'])
+            first_record = PersonAttend.objects.filter(
+                person=person, time_sent=record['first_seen_time']
+            ).first()
+            last_record = PersonAttend.objects.filter(
+                person=person, time_sent=record['last_seen_time']
+            ).first()
+
+            person_data.append({
+                "id": person.id,
+                "full_name": person.full_name,
+                "first": {
+                    "id": first_record.id,
+                    "img_url": first_record.img_url,
+                    "camera_id": first_record.camera_id,
+                    "time_sent": first_record.time_sent
+                    },
+                "last": {
+                    "id": last_record.id,
+                    "img_url": last_record.img_url,
+                    "camera_id": last_record.camera_id,
+                    "time_sent": last_record.time_sent
+                }
+            })
+
+        # Paginate the person_data list
+        paginator = Paginator(person_data, 10)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
 
@@ -31,6 +65,3 @@ class Report(TemplateView):
             "end_date": end_date_str
         }
         return render(request, self.template_name, context)
-
-    def post(self, request):
-        pass
--- configurations/settings.py
+++ configurations/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'configurations.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'faceR',
-        'USER': 'myuser',
-        'PASSWORD': 'gil12345',
+        'NAME': 'face_full',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
